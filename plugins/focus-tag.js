7f582a8c68abe943e9c72258dbd8e4cc98bdddbc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-11-17 17:01:11 +0800

    Deploy 4everlynn/fatewa to github.com/4everlynn/fatewa.git:gh-pages

--- plugins/focus-tag.js
+++ plugins/focus-tag.js
@@ -4,15 +4,16 @@ class FocusTag extends HTMLElement {
         const shadowRoot = this.attachShadow({mode: 'open'});
         let hasLink = this.hasAttribute("link");
         shadowRoot.innerHTML = `
-            <div style="background: ${hasLink ? '#07163a' : '#5319f5' };
+            <div style="background: ${hasLink ? '#07163a' : 'linear-gradient(45deg, #5319f5, #96002a54)' };
                          display: inline-block;
                          padding: 3px 8px;
                          border-radius: 3px;
                          color: white;
                          letter-spacing: 2px;
                          border: 1px solid #642bff;
-                         ${hasLink ? 'border-bottom: 1px solid #00ff5e;' : null }
-                         margin: 8px 3px;
+                         ${hasLink ? 'border-bottom: 1px solid #00ff5e;' : 'border-bottom: none;' }
+                         margin: 8px 8px;
+                         transform: translateY(-3px);
                          font-weight: 200;
                          font-size: 12px">
                  ${this.innerHTML}
